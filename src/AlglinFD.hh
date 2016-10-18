112b4871d669e713c6fb10f2e985e2d270ab96be
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2016-10-18 22:36:48 +0200

    Added AlglinFD.hh finite difference support for gradient, jacobian and Hessian computation

---
+++ src/AlglinFD.hh
@@ -0,0 +1,423 @@
+/*--------------------------------------------------------------------------*\
+ |                                                                          |
+ |  Copyright (C) 2008-2015                                                 |
+ |                                                                          |
+ |         , __                 , __                                        |
+ |        /|/  \               /|/  \                                       |
+ |         | __/ _   ,_         | __/ _   ,_                                |
+ |         |   \|/  /  |  |   | |   \|/  /  |  |   |                        |
+ |         |(__/|__/   |_/ \_/|/|(__/|__/   |_/ \_/|/                       |
+ |                           /|                   /|                        |
+ |                           \|                   \|                        |
+ |                                                                          |
+ |      Enrico Bertolazzi                                                   |
+ |      Dipartimento di Ingegneria Industriale                              |
+ |      Universita` degli Studi di Trento                                   |
+ |      email: enrico.bertolazzi@unitn.it                                   |
+ |                                                                          |
+\*--------------------------------------------------------------------------*/
+
+///
+/// file: alglinFD.hh
+///
+
+#include "Alglin.hh"
+#include <iostream>
+#include <iomanip>
+
+#ifndef ALGLIN_FD_HH
+#define ALGLIN_FD_HH
+
+namespace alglin {
+
+  /*
+  //    ____               _ _            _
+  //   / ___|_ __ __ _  __| (_) ___ _ __ | |_
+  //  | |  _| '__/ _` |/ _` | |/ _ \ '_ \| __|
+  //  | |_| | | | (_| | (_| | |  __/ | | | |_
+  //   \____|_|  \__,_|\__,_|_|\___|_| |_|\__|
+  */
+  template <typename FUNCTION, typename Number>
+  inline
+  bool
+  finite_difference_gradient( integer      fd_gradient,
+                              Number const x[],
+                              integer      dim_x,
+                              FUNCTION     fun,
+                              Number       grad[] ) {
+
+    Number const eps = sqrt(std::numeric_limits<Number>::epsilon()) ;
+
+    Number value0=0, value1=0 ; // only to stop warning
+    bool ok = true ;
+    if ( fd_gradient != 0 ) {
+      ok = fun( x, value0 ) && isRegular(value0) ;
+      if ( !ok ) return false ;
+    }
+
+    Number * X = const_cast<Number*>(x) ;
+
+    for ( integer i = 0 ; i < dim_x && ok ; ++i ) {
+      Number temp = x[i] ;
+      Number h    = std::max( eps*std::abs(temp), eps ) ;
+      switch ( fd_gradient ) {
+      case 0:
+        X[i] = temp+h ; // modify the vector only at i position
+        ok = fun( X, value1 ) && isRegular(value1) ;
+        if ( !ok ) break ;
+
+        X[i] = temp-h ; // modify the vector only at i position
+        ok = fun( X, value0 ) && isRegular(value0) ;
+        if ( ok ) grad[i] = (value1-value0)/(2*h) ;
+        break ;
+
+      case 1:
+        X[i] = temp+h ; // modify the vector only at i position
+        ok = fun( X, value1 ) && isRegular(value1) ;
+        if ( ok ) grad[i] = (value1-value0)/h ;
+        break ;
+    
+      case -1:
+        X[i] = temp-h ; // modify the vector only at i position
+        ok = fun( X, value1 ) && isRegular(value1) ;
+        if ( ok ) grad[i] = (value0-value1)/h ;
+        break ;
+      default:
+        ALGLIN_ERROR( "finite_difference_gradient::fd_gradient = " <<
+                      fd_gradient << " must be in {-1, 0, 1}" ) ;
+      }
+      X[i] = temp ; // restore i position
+      ok = isRegular(grad[i]) ;
+    }
+    return ok ;
+  }
+
+  template <typename FUNCTION, typename Number>
+  inline
+  void
+  finite_difference_check_gradient( Number const   x[],
+                                    integer        dim_x,
+                                    FUNCTION       fun,
+                                    Number const   grad[],
+                                    Number         epsi,
+                                    std::ostream & stream ) {
+
+    Number const eps = sqrt(std::numeric_limits<Number>::epsilon()) ;
+    Number * X = const_cast<Number*>(x) ;
+
+    bool ok = true ;
+    Number value0, value1, gradi ;
+    for ( integer i = 0 ; i < dim_x && ok ; ++i ) {
+      Number temp = x[i] ;
+      Number h    = std::max( eps*std::abs(temp), eps ) ;
+      X[i] = temp+h ; // modify the vector only at i position
+      ok = fun( X, value1 ) && isRegular(value1) ;
+      if ( !ok ) break ;
+
+      X[i] = temp-h ; // modify the vector only at i position
+      ok = fun( X, value0 ) && isRegular(value0) ;
+      if ( ok ) gradi = (value1-value0)/(2*h) ;
+
+      X[i] = temp ; // restore i position
+      ok = isRegular(gradi) ;
+      if ( ok ) {
+        Number scale = std::max(Number(1),std::max(std::abs(gradi),std::abs(grad[i]))) ;
+        Number err   = std::abs(gradi-grad[i]) ;
+        if ( err > epsi*scale ) {
+          stream << "grad[" << i << "] = " << setw(14) << grad[i] << " --- "
+                 << setw(14) << gradi << "  rel err = " << err/scale
+                 << '\n' ;
+        }
+      }
+    }
+    if ( !ok )
+      stream << "failed finite difference in `finite_difference_check_gradient`\n" ;
+  }
+
+  // ---------------------------------------------------------------------------
+
+  template <typename Number>
+  inline
+  bool
+  isRegular( Number const v[], integer n ) {
+    bool ok = true ;
+    for ( integer i = 0 ; i < n && ok ; ++i ) ok = alglin::isRegular(v[i]) ;
+    return ok ;
+  }
+
+  /*
+  //       _                 _     _
+  //      | | __ _  ___ ___ | |__ (_) __ _ _ __
+  //   _  | |/ _` |/ __/ _ \| '_ \| |/ _` | '_ \
+  //  | |_| | (_| | (_| (_) | |_) | | (_| | | | |
+  //   \___/ \__,_|\___\___/|_.__/|_|\__,_|_| |_|
+  */
+  template <typename FUNCTION, typename Number>
+  bool
+  finite_difference_jacobian( integer      fd_jacobian,
+                              Number const x[],
+                              integer      dim_x,
+                              FUNCTION     fun,
+                              integer      dim_f,
+                              Number       Jac[],
+                              integer      ldJ ) {
+
+    Number const eps = sqrt(std::numeric_limits<Number>::epsilon()) ;
+
+    std::vector<Number> tmp(size_t(2*dim_f)) ;
+    Number * g0 = &tmp[size_t(0)] ;
+    Number * g1 = &tmp[size_t(dim_f)] ;
+    bool ok = true ;
+
+    if ( fd_jacobian != 0 ) {
+      ok = fun( x, g0 ) && isRegular(g0,dim_f) ;
+      if ( !ok ) return false ;
+    }
+
+    Number * X = const_cast<Number*>(x) ;
+    Number * pjac = Jac ;
+
+    for ( integer j = 0 ; j < dim_x && ok ; ++j ) {
+      Number temp = x[j] ;
+      Number h    = std::max( eps*std::abs(temp), eps ) ;
+      switch ( fd_jacobian ) {
+      case 0:
+        X[j] = temp+h ; // modify the vector only at j position
+        ok = fun( X, g1 ) && isRegular(g1,dim_f) ;
+        if ( !ok ) break ;
+
+        X[j] = temp-h ; // modify the vector only at j position
+        ok = fun( X, g0 ) && isRegular(g0,dim_f) ;
+        if ( ok )
+          for ( integer i = 0 ; i < dim_f ; ++i )
+            pjac[i] = (g1[i]-g0[i])/(2*h) ;
+        break ;
+
+      case 1:
+        X[j] = temp+h ; // modify the vector only at j position
+        ok = fun( X, g1 ) && isRegular(g1,dim_f) ;
+        if ( ok )
+          for ( integer i = 0 ; i < dim_f ; ++i )
+            pjac[i] = (g1[i]-g0[i])/h ;
+        break ;
+    
+      case -1:
+        X[j] = temp-h ; // modify the vector only at i position
+        ok = fun( X, g1 ) && isRegular(g1,dim_f) ;
+        if ( ok )
+          for ( integer i = 0 ; i < dim_f ; ++i )
+            pjac[j] = (g0[i]-g1[i])/h ;
+        break ;
+      default:
+        ALGLIN_ERROR( "finite_difference_jacobian::fd_gradient = " <<
+                      fd_jacobian << " must be in {-1, 0, 1}" ) ;
+      }
+      X[j] = temp ; // modify the vector only at i position
+      ok = isRegular(pjac,dim_f) ;
+      pjac += ldJ ;
+    }
+    return ok ;
+  }
+
+  template <typename FUNCTION, typename Number>
+  void
+  finite_difference_check_jacobian( Number const   x[],
+                                    integer        dim_x,
+                                    FUNCTION       fun,
+                                    integer        dim_f,
+                                    Number const   Jac[],
+                                    integer        ldJ,
+                                    Number         epsi,
+                                    std::ostream & stream  ) {
+
+    Number const eps = sqrt(std::numeric_limits<Number>::epsilon()) ;
+
+    std::vector<Number> tmp(size_t(2*dim_f)) ;
+    Number * g0 = &tmp[size_t(0)] ;
+    Number * g1 = &tmp[size_t(dim_f)] ;
+    bool ok = true ;
+
+    Number       * X    = const_cast<Number*>(x) ;
+    Number const * pjac = Jac ;
+
+    for ( integer j = 0 ; j < dim_x && ok ; ++j ) {
+      Number temp = x[j] ;
+      Number h    = std::max( eps*std::abs(temp), eps ) ;
+
+      X[j] = temp+h ; // modify the vector only at j position
+      ok = fun( X, g1 ) && isRegular(g1,dim_f) ;
+      if ( !ok ) break ;
+
+      X[j] = temp-h ; // modify the vector only at j position
+      ok = fun( X, g0 ) && isRegular(g0,dim_f) ;
+
+      for ( integer i = 0 ; i < dim_f && ok ; ++i ) {
+        ok = isRegular(pjac,dim_f) ;
+        Number d     = (g1[i]-g0[i])/(2*h) ;
+        Number scale = std::max(Number(1),std::max(std::abs(d),std::abs(pjac[i]))) ;
+        Number err   = std::abs(d-pjac[i]) ;
+        if ( err > epsi*scale ) {
+          stream << "jac[" << i << ", " << j << "] = " << setw(14) << pjac[i] << " --- "
+                 << setw(14) << d << "  rel err = " << err/scale
+                 << '\n' ;
+        }
+      }
+
+      X[j] = temp ; // modify the vector only at i position
+      pjac += ldJ ;
+    }
+    if ( !ok )
+      stream << "failed finite difference in `finite_difference_check_jacobian`\n" ;
+  }
+
+  /*
+  //   _   _               _
+  //  | | | | ___  ___ ___(_) __ _ _ __
+  //  | |_| |/ _ \/ __/ __| |/ _` | '_ \
+  //  |  _  |  __/\__ \__ \ | (_| | | | |
+  //  |_| |_|\___||___/___/_|\__,_|_| |_|
+  */
+  template <typename FUNCTION, typename Number>
+  bool
+  finite_difference_hessian( Number const x[],
+                             integer      dim_x,
+                             FUNCTION     fun,
+                             Number       Hess[],
+                             integer      ldH ) {
+
+    Number const eps = pow(std::numeric_limits<Number>::epsilon(),0.25) ;
+    bool ok = true ;
+
+    Number * X = const_cast<Number*>(x) ;
+    Number fpp, fpm, fmp, fmm ;
+    for ( integer j = 0 ; j < dim_x && ok ; ++j ) {
+      Number tempj = x[j] ;
+      Number hj    = std::max( eps*std::abs(tempj), eps ) ;
+      
+      Number fp, fm, fc ;
+      ok = fun( X, fc ) && alglin::isRegular(fc) ;
+      if ( !ok ) break ;
+      X[j] = tempj+hj ;
+      ok = fun( X, fp ) && alglin::isRegular(fp) ;
+      if ( !ok ) break ;
+      X[j] = tempj-hj ;
+      ok = fun( X, fm ) && alglin::isRegular(fm) ;
+      if ( !ok ) break ;
+      Hess[j*(ldH+1)] = ((fp+fm)-2*fc)/(hj*hj) ;
+      for ( integer i = j+1 ; i < dim_x && ok ; ++i ) {
+        Number tempi = X[i] ;
+        Number hi    = std::max( eps*std::abs(tempi), eps ) ;
+        X[i] = tempi+hi ;
+        X[j] = tempj+hj ;
+        ok = fun( X, fpp ) && alglin::isRegular(fpp) ;
+        if ( !ok ) break ;
+        X[i] = tempi-hi ;
+        ok = fun( X, fmp ) && alglin::isRegular(fmp) ;
+        if ( !ok ) break ;
+        X[j] = tempj-hj ;
+        ok = fun( X, fmm ) && alglin::isRegular(fmm) ;
+        if ( !ok ) break ;
+        X[i] = tempi+hi ;
+        ok = fun( X, fpm ) && alglin::isRegular(fpm) ;
+        if ( !ok ) break ;
+        Number hij = 4*hi*hj ;
+        Hess[j+i*ldH] = Hess[i+j*ldH] = ( (fpp+fmm) - (fpm+fmp) )/hij ;
+        X[i] = tempi ;
+      }
+      X[j] = tempj ;
+    }
+    return ok ;
+  }
+  
+  
+  template <typename FUNCTION, typename Number>
+  bool
+  finite_difference_check_hessian( Number const   x[],
+                                   integer        dim_x,
+                                   FUNCTION       fun,
+                                   Number const   Hess[],
+                                   integer        ldH,
+                                   Number         epsi,
+                                   std::ostream & stream ) {
+
+    Number const eps = pow(std::numeric_limits<Number>::epsilon(),0.25) ;
+    bool ok = true ;
+
+    Number * X = const_cast<Number*>(x) ;
+    Number fpp, fpm, fmp, fmm ;
+    for ( integer j = 0 ; j < dim_x && ok ; ++j ) {
+      Number tempj = x[j] ;
+      Number hj    = std::max( eps*std::abs(tempj), eps ) ;
+      
+      Number fp, fm, fc ;
+      ok = fun( X, fc ) && alglin::isRegular(fc) ;
+      if ( !ok ) break ;
+      X[j] = tempj+hj ;
+      ok = fun( X, fp ) && alglin::isRegular(fp) ;
+      if ( !ok ) break ;
+      X[j] = tempj-hj ;
+      ok = fun( X, fm ) && alglin::isRegular(fm) ;
+      if ( !ok ) break ;
+
+      Number dde = Hess[j*(ldH+1)] ;
+      Number dd  = ((fp+fm)-2*fc)/(hj*hj) ;
+      ok = alglin::isRegular(dd) ;
+      if ( !ok ) break ;
+      Number scale = std::max(Number(1),std::max(std::abs(dd),std::abs(dde))) ;
+      Number err   = std::abs(dd-dde) ;
+      if ( err > epsi*scale ) {
+        stream << "Hess[" << j << ", " << j << "] = " << setw(14) << dde << " --- "
+               << setw(14) << dd << "  rel err = " << err/scale
+               << '\n' ;
+      }
+
+      for ( integer i = j+1 ; i < dim_x && ok ; ++i ) {
+        Number tempi = X[i] ;
+        Number hi    = std::max( eps*std::abs(tempi), eps ) ;
+        X[i] = tempi+hi ;
+        X[j] = tempj+hj ;
+        ok = fun( X, fpp ) && alglin::isRegular(fpp) ;
+        if ( !ok ) break ;
+        X[i] = tempi-hi ;
+        ok = fun( X, fmp ) && alglin::isRegular(fmp) ;
+        if ( !ok ) break ;
+        X[j] = tempj-hj ;
+        ok = fun( X, fmm ) && alglin::isRegular(fmm) ;
+        if ( !ok ) break ;
+        X[i] = tempi+hi ;
+        ok = fun( X, fpm ) && alglin::isRegular(fpm) ;
+        if ( !ok ) break ;
+        Number hij  = 4*hi*hj ;
+        Number ddji = Hess[j+i*ldH] ;
+        Number ddij = Hess[i+j*ldH] ;
+        dd   = ( (fpp+fmm) - (fpm+fmp) )/hij ;
+        ok = alglin::isRegular(dd) ;
+        if ( !ok ) break ;
+        scale = std::max(Number(1),std::max(std::abs(dd),std::abs(ddij))) ;
+        err   = std::abs(dd-ddij) ;
+        if ( err > epsi*scale ) {
+          stream << "Hess[" << i << ", " << j << "] = " << setw(14) << ddij << " --- "
+                 << setw(14) << dd << "  rel err = " << err/scale
+                 << '\n' ;
+        }
+        scale = std::max(Number(1),std::max(std::abs(dd),std::abs(ddji))) ;
+        err   = std::abs(dd-ddji) ;
+        if ( err > epsi*scale ) {
+          stream << "Hess[" << j << ", " << i << "] = " << setw(14) << ddij << " --- "
+                 << setw(14) << dd << "  rel err = " << err/scale
+                 << '\n' ;
+        }
+        X[i] = tempi ;
+      }
+      X[j] = tempj ;
+    }
+    return ok ;
+  }
+
+} // end namespace alglin
+
+#endif
+
+///
+/// eof: AlglinFD.hh
+///
---
+++ src_tests/TestFD.cc
@@ -0,0 +1,144 @@
+/*--------------------------------------------------------------------------*\
+ |                                                                          |
+ |  Copyright (C) 2008-2015                                                 |
+ |                                                                          |
+ |         , __                 , __                                        |
+ |        /|/  \               /|/  \                                       |
+ |         | __/ _   ,_         | __/ _   ,_                                |
+ |         |   \|/  /  |  |   | |   \|/  /  |  |   |                        |
+ |         |(__/|__/   |_/ \_/|/|(__/|__/   |_/ \_/|/                       |
+ |                           /|                   /|                        |
+ |                           \|                   \|                        |
+ |                                                                          |
+ |      Enrico Bertolazzi                                                   |
+ |      Dipartimento di Ingegneria Industriale                              |
+ |      Universita` degli Studi di Trento                                   |
+ |      email: enrico.bertolazzi@unitn.it                                   |
+ |                                                                          |
+\*--------------------------------------------------------------------------*/
+
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <random>
+#include "AlglinFD.hh"
+
+using namespace alglin ;
+
+static
+bool
+fun( doublereal const x[4], doublereal & res ) {
+  res = x[0] + sin(x[1]) + exp(x[2]*x[3]) + x[0]*x[1]*x[2] ;
+  return true ;
+}
+
+static
+bool
+fun_grad( doublereal const x[4], doublereal grad[4] ) {
+  grad[0] = 1+x[1]*x[2] ;
+  grad[1] = cos(x[1])+ x[0]*x[2]  ;
+  grad[2] = exp(x[2]*x[3])*x[3]+x[0]*x[1] ;
+  grad[3] = exp(x[2]*x[3])*x[2] ;
+  return true ;
+}
+
+static
+bool
+fun_jac( doublereal const x[4], doublereal jac[4*4] ) {
+
+  jac[0+0*4] = 0 ;
+  jac[0+1*4] = x[2] ;
+  jac[0+2*4] = x[1] ;
+  jac[0+3*4] = 0 ;
+  
+  jac[1+0*4] = x[2] ;
+  jac[1+1*4] = -sin(x[1]) ;
+  jac[1+2*4] = x[0] ;
+  jac[1+3*4] = 0 ;
+
+  jac[2+0*4] = x[1] ;
+  jac[2+1*4] = x[0] ;
+  jac[2+2*4] = exp(x[2]*x[3])*x[3]*x[3] ;
+  jac[2+3*4] = exp(x[2]*x[3]) + exp(x[2]*x[3])*x[2]*x[3]  ;
+
+  jac[3+0*4] = 0 ;
+  jac[3+1*4] = 0 ;
+  jac[3+2*4] = exp(x[2]*x[3])+exp(x[2]*x[3])*x[2]*x[3] ;
+  jac[3+3*4] = exp(x[2]*x[3])*x[2]*x[2] ;
+
+  return true ;
+}
+
+class fun_class {
+public:
+  bool operator () ( doublereal const x[4], doublereal & res ) {
+    return fun(x,res) ;
+  }
+} ;
+
+class fun_class1 {
+public:
+  bool operator () ( doublereal const x[4], doublereal res[4] ) {
+    return fun_grad( x, res ) ;
+  }
+} ;
+
+int
+main() {
+
+  integer    dim_x = 4 ;
+  doublereal x[4] = { 1, 2, 1, 4 } ;
+  doublereal gradFD[4], grad[4], jac[4*4], jacFD[4*4] ;
+
+  fun_class  ff ;
+  fun_class1 gg ;
+
+  bool ok = finite_difference_gradient( 0, x, dim_x, ff, gradFD ) ;
+  ok = fun_grad( x, grad ) ;
+  doublereal epsi = 1e-6 ;
+  cout << "\n\nCheck Gradient\n" ;
+  finite_difference_check_gradient( x, dim_x, ff, grad, epsi, cout ) ;
+  cout << "Done\n" ;
+
+  cout << "diff grad(FD)[0] = " << gradFD[0] - grad[0] << '\n' ;
+  cout << "diff grad(FD)[1] = " << gradFD[1] - grad[1] << '\n' ;
+  cout << "diff grad(FD)[2] = " << gradFD[2] - grad[2] << '\n' ;
+  cout << "diff grad(FD)[3] = " << gradFD[3] - grad[3] << '\n' ;
+  cout << "diff grad(FD)[0] = " << (gradFD[0] - grad[0])/max(1.0,abs(grad[0])) << '\n' ;
+  cout << "diff grad(FD)[1] = " << (gradFD[1] - grad[1])/max(1.0,abs(grad[1])) << '\n' ;
+  cout << "diff grad(FD)[2] = " << (gradFD[2] - grad[2])/max(1.0,abs(grad[2])) << '\n' ;
+  cout << "diff grad(FD)[3] = " << (gradFD[3] - grad[3])/max(1.0,abs(grad[3])) << '\n' ;
+
+  ok = fun_jac( x, jac ) ;
+  ok = finite_difference_jacobian( 0, x, dim_x, gg, dim_x, jacFD, dim_x ) ;
+  cout << "\n\nCheck Jacobian\n" ;
+  finite_difference_check_jacobian( x, dim_x, gg, dim_x, jac, dim_x, epsi, cout ) ;
+  cout << "Done\n" ;
+
+  for ( int i = 0 ; i < dim_x ; ++i ) {
+    for ( int j = 0 ; j < dim_x ; ++j ) {
+      cout << "jac[" << i << "," << j << "] = " << setw(14) << jac[i+j*dim_x]
+           << " err = " << abs(jac[i+j*dim_x]-jacFD[i+j*dim_x])
+           << "\n" ;
+    }
+  }
+
+  ok = finite_difference_hessian( x, dim_x, ff, jacFD, dim_x ) ;
+  cout << "ok = " << (ok?"TRUE\n":"FALSE\n") ;
+  for ( int i = 0 ; i < dim_x ; ++i ) {
+    for ( int j = 0 ; j < dim_x ; ++j ) {
+      cout << "Hess[" << i << "," << j << "] = " << setw(14) << jac[i+j*dim_x]
+           << " HessFD[" << i << "," << j << "] = " << setw(14) << jacFD[i+j*dim_x]
+           << " err = " << abs(jac[i+j*dim_x]-jacFD[i+j*dim_x])
+           << "\n" ;
+    }
+  }
+
+  cout << "\n\nCheck Hessian\n" ;
+  finite_difference_check_hessian( x, dim_x, ff, jac, dim_x, epsi, cout ) ;
+  cout << "Done\n" ;
+
+  cout << "All done!\n" ;
+
+  return 0 ;
+}
