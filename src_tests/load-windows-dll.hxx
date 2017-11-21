46e7410a7584968b20f21b262d266adf80fa113f
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2017-11-22 00:14:12 +0100

    updated windows support (3)

--- src_tests/load-windows-dll.hxx
+++ src_tests/load-windows-dll.hxx
@@ -17,47 +17,69 @@
  |                                                                          |
 \*--------------------------------------------------------------------------*/
 
+#define DLL_BASE_PATH "../lib3rd"
+
 #ifdef ALGLIN_OS_WINDOWS
 
-  #define DLL_BASE_PATH "../lib3rd"
+  #ifdef __cplusplus
+  std::cout << "load dlls\n" ;
+  #else
+  printf("load dlls\n") ;
+  #endif
+
+  #if 1
+
+  AddDllDirectory( L"../lib3rd/lapack/dll/" ) ;
+  AddDllDirectory( L"../lib3rd/openblas/dll/" ) ;
+  AddDllDirectory( L"../lib3rd/superlu/dll/" ) ;
+
+  #else
 
   HMODULE myDll ;
 
   #if defined(ALGLIN_USE_LAPACK)
     #if defined(_DEBUG) || defined(DEBUG)
       #ifdef ALGLIN_ARCH64
-        myDll = LoadLibrary(DLL_BASE_PATH "/lapack/dll/blas_win64_MTd.dll");
+        myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/lapack/dll/blas_win64_MTd.dll"));
         ALGLIN_ASSERT( myDll != nullptr, "Falling in loading blas_win64_MTd.dll");
-        myDll = LoadLibrary(DLL_BASE_PATH "/lapack/dll/lapack_win64_MTd.dll");
+        myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/lapack/dll/lapack_win64_MTd.dll"));
         ALGLIN_ASSERT( myDll != nullptr, "Falling in loading lapack_win64_MTd.dll");
       #else
-        myDll = LoadLibrary(DLL_BASE_PATH "/lapack/dll/blas_win32_MTd.dll");
+        myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/lapack/dll/blas_win32_MTd.dll"));
         ALGLIN_ASSERT( myDll != nullptr, "Falling in loading blas_win32_MTd.dll");
-        myDll = LoadLibrary(DLL_BASE_PATH "/lapack/dll/lapack_win32_MTd.dll");
+        myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/lapack/dll/lapack_win32_MTd.dll"));
         ALGLIN_ASSERT( myDll != nullptr, "Falling in loading lapack_win32_MTd.dll");
       #endif
     #else
       #ifdef ALGLIN_ARCH64
-        myDll = LoadLibrary(DLL_BASE_PATH "/lapack/dll/blas_win64_MT.dll");
+        myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/lapack/dll/blas_win64_MT.dll"));
         ALGLIN_ASSERT( myDll != nullptr, "Falling in loading blas_win64_MT.dll");
-        myDll = LoadLibrary(DLL_BASE_PATH "/lapack/dll/lapack_win64_MT.dll");
+        myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/lapack/dll/lapack_win64_MT.dll"));
         ALGLIN_ASSERT( myDll != nullptr, "Falling in loading lapack_win64_MT.dll");
       #else
-        myDll = LoadLibrary(DLL_BASE_PATH "/lapack/dll/blas_win32_MT.dll");
+        myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/lapack/dll/blas_win32_MT.dll"));
         ALGLIN_ASSERT( myDll != nullptr, "Falling in loading blas_win32_MT.dll");
-        myDll = LoadLibrary(DLL_BASE_PATH "/lapack/dll/lapack_win32_MT.dll");
+        myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/lapack/dll/lapack_win32_MT.dll"));
         ALGLIN_ASSERT( myDll != nullptr, "Falling in loading lapack_win32_MT.dll");
       #endif
     #endif
   #elif defined(ALGLIN_USE_OPENBLAS)
     // no debug version
     #ifdef ALGLIN_ARCH64
-      myDll = LoadLibrary(DLL_BASE_PATH "/openblas/dll/libopenblas_x64.dll");
+      myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/openblas/dll/libopenblas_x64.dll"));
       ALGLIN_ASSERT( myDll != nullptr, "Falling in loading libopenblas_x64.dll");
     #else
-      myDll = LoadLibrary(DLL_BASE_PATH "/openblas/dll/libopenblas_x86.dll");
+      myDll = LoadLibrary(TEXT(DLL_BASE_PATH "/openblas/dll/libopenblas_x86.dll"));
       ALGLIN_ASSERT( myDll != nullptr, "Falling in loading libopenblas_x86.dll");
     #endif
+  #endif
+
+  #endif
+
+  #ifdef __cplusplus
+  std::cout << "done load dlls\n" ;
+  #else
+  printf("done load dlls\n") ;
   #endif
 
 #endif
