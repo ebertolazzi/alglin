b73ca3190653d78b53fb9c08e6d37dff482c7b3d
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2019-03-20 20:49:06 +0100

    Updated SparseBase and SparseCCOOR

--- src/Alglin_sparse.hxx
+++ src/Alglin_sparse.hxx
@@ -25,10 +25,14 @@
 
 namespace alglin {
 
+  template <typename T> class MatrixWrapper;
+
   template <typename T>
   class SparseMatrixBase {
   public:
-    typedef T valueType;
+    typedef T                   valueType;
+    typedef SparseMatrixBase<T> Sparse;
+    typedef MatrixWrapper<T>    MatW;
   protected:
 
     /*!
@@ -39,10 +43,10 @@ namespace alglin {
 
     void
     y_manage(
-      valueType       beta,
-      integer         DimY,
-      valueType       y[],
-      integer         incY
+      valueType beta,
+      integer   DimY,
+      valueType y[],
+      integer   incY
     ) const {
       if ( isZero(beta) ) {
         for ( integer i = 0; i < DimY; ++i ) y[i*incY] = 0;
@@ -60,10 +64,37 @@ namespace alglin {
     virtual
     ~SparseMatrixBase() {}
 
+    /*!
+     *  \brief Initialize sparse matrix
+     *
+     *  \param[in] N           number of rows
+     *  \param[in] M           number of columns
+     *  \param[in] reserve_nnz estimated number of nonzeros
+     *  \param[in] fi          if true use FORTRAN 1-based indices
+    \*/
+    virtual
+    void
+    init(
+      integer N,
+      integer M,
+      integer reserve_nnz,
+      bool    fi = false
+    ) ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief Destroy the sparsde matrix
+     *
+    \*/
+    virtual
+    void
+    clear() ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief Return true if index are 1-based
+    \*/
     virtual
     bool
-    FORTRAN_indexing() const
-    { return false; }
+    FORTRAN_indexing() const ALGLIN_PURE_VIRTUAL;
 
     virtual
     integer
@@ -126,6 +157,134 @@ namespace alglin {
     ) const ALGLIN_PURE_VIRTUAL;
 
     /*!
+     *  \brief transpose the matrix
+    \*/
+    virtual
+    void
+    transpose() ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief convert the matrix to 1-based index
+    \*/
+    virtual
+    void
+    to_FORTRAN_indexing() ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief convert the matrix to 0-based index
+    \*/
+    virtual
+    void
+    to_C_indexing() ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief insert a value into matrix using 0-based index
+     *
+     *  \param[in] row the row index
+     *  \param[in] col the column index
+     *  \param[in] val the inserted value
+     *
+    \*/
+    virtual
+    void
+    push_value_C(
+      integer   row,
+      integer   col,
+      valueType val
+    ) ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief insert a value into matrix using 1-based index
+     *
+     *  \param[in] row the row index
+     *  \param[in] col the column index
+     *  \param[in] val the inserted value
+     *
+    \*/
+    virtual
+    void
+    push_value_F(
+      integer   row,
+      integer   col,
+      valueType val
+    ) ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief insert a matrix into the sparse matrix
+     *
+     *  \param[in] row_offs  offset to the row indices
+     *  \param[in] col_offs  offset to the column indices
+     *  \param[in] Matrix    the (full) matrix to be inserted
+     *  \param[in] transpose if true matrix is inserted transposed
+     *
+    \*/
+    virtual
+    void
+    push_matrix(
+      integer      row_offs,
+      integer      col_offs,
+      MatW const & Matrix,
+      bool         transpose = false
+    ) ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief insert a matrix into the sparse matrix
+     *
+     *  \param[in] row_offs  offset to the row indices
+     *  \param[in] col_offs  offset to the column indices
+     *  \param[in] Matrix    the (sparsse) matrix to be inserted
+     *  \param[in] transpose if true matrix is inserted transposed
+     *
+    \*/
+    virtual
+    void
+    push_matrix(
+      integer        row_offs,
+      integer        col_offs,
+      Sparse const & Matrix,
+      bool           transpose = false
+    ) ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief extract the sparse matrix to a full matrix
+     *
+     *  \param[out] M the output matrix
+     *
+    \*/
+    virtual
+    void
+    get_matrix( MatW & M ) const ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief extract the sparse matrix to a full matrix
+     *         filling symmetrically
+     *
+     *  \param[out] M the output matrix
+     *
+    \*/
+    virtual
+    void
+    get_matrix_symmetric( MatW & M ) const ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \brief extract the sparse matrix to a full matrix
+     *         trasposing the result
+     *
+     *  \param[out] M the output matrix
+     *
+    \*/
+    virtual
+    void
+    get_matrix_transposed( MatW & M ) const ALGLIN_PURE_VIRTUAL;
+
+    /*!
+     *  \return true if Inf or NaN are found in the sparse data
+    \*/
+    virtual
+    bool
+    foundNaN() const ALGLIN_PURE_VIRTUAL;
+
+    /*!
      * \brief gemv:
      *        Calls the blas-Routine (\f$y = \beta y + \alpha A x + y\f$).
      *
@@ -156,14 +315,14 @@ namespace alglin {
      * \brief gemv:
      *        Calls the blas-Routine (\f$y = \beta y + \alpha A^T x + y\f$).
      *
-     * \param[in]     alpha         Scalar \f$\alpha \f$.
-     * \param[in]     DimX          Dimension of the vector x.
-     * \param[in]     x             Vector x.
-     * \param[in]     incX          Stride of vector x.
-     * \param[in]     DimY          Dimension of the vector y.
-     * \param[in]     beta          Scalar \f$\beta \f$.
-     * \param[in,out] y             In-/Output vector y.
-     * \param[in]     incY          Stride of vector y.
+     * \param[in]     alpha Scalar \f$\alpha \f$.
+     * \param[in]     DimX  Dimension of the vector x.
+     * \param[in]     x     Vector x.
+     * \param[in]     incX  Stride of vector x.
+     * \param[in]     DimY  Dimension of the vector y.
+     * \param[in]     beta  Scalar \f$\beta \f$.
+     * \param[in,out] y     In-/Output vector y.
+     * \param[in]     incY  Stride of vector y.
      *
     \*/
 
@@ -184,14 +343,14 @@ namespace alglin {
      * \brief gemv:
      *        Calls the blas-Routine (\f$y = \beta y + \alpha (A+A^T-\textrm{diag}(A)) x + y\f$).
      *
-     * \param[in]     alpha         Scalar \f$\alpha \f$.
-     * \param[in]     DimX          Dimension of the vector x.
-     * \param[in]     x             Vector x.
-     * \param[in]     incX          Stride of vector x.
-     * \param[in]     DimY          Dimension of the vector y.
-     * \param[in]     beta          Scalar \f$\beta \f$.
-     * \param[in,out] y             In-/Output vector y.
-     * \param[in]     incY          Stride of vector y.
+     * \param[in]     alpha Scalar \f$\alpha \f$.
+     * \param[in]     DimX  Dimension of the vector x.
+     * \param[in]     x     Vector x.
+     * \param[in]     incX  Stride of vector x.
+     * \param[in]     DimY  Dimension of the vector y.
+     * \param[in]     beta  Scalar \f$\beta \f$.
+     * \param[in,out] y     In-/Output vector y.
+     * \param[in]     incY  Stride of vector y.
      *
     \*/
     virtual
@@ -217,8 +376,6 @@ namespace alglin {
   //  |____/| .__/ \__,_|_|  |___/\___|\____\____\___/ \___/|_| \_\
   //        |_|
   */
-
-  template <typename T> class MatrixWrapper;
 
   //! Sparse Matrix Structure
   template <typename T>
@@ -259,6 +416,19 @@ namespace alglin {
     {}
 
     virtual
+    void
+    clear() ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    init(
+      integer N,
+      integer M,
+      integer reserve_nnz,
+      bool    fi = false
+    ) ALGLIN_OVERRIDE;
+
+    virtual
     bool
     FORTRAN_indexing() const ALGLIN_OVERRIDE
     { return this->fortran_indexing; }
@@ -280,6 +450,71 @@ namespace alglin {
 
     virtual
     void
+    transpose() ALGLIN_OVERRIDE {
+      this->rows.swap(this->cols);
+      std::swap( this->nRows, this->nCols );
+    }
+
+    virtual
+    void
+    to_FORTRAN_indexing() ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    to_C_indexing() ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    push_value_C(
+      integer   row,
+      integer   col,
+      valueType val
+    ) ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    push_value_F(
+      integer   row,
+      integer   col,
+      valueType val
+    ) ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    push_matrix(
+      integer      row_offs,
+      integer      col_offs,
+      MatW const & Matrix,
+      bool         transpose = false
+    ) ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    push_matrix(
+      integer        row_offs,
+      integer        col_offs,
+      Sparse const & Matrix,
+      bool           transpose = false
+    ) ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    get_matrix( MatW & M ) const ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    get_matrix_symmetric( MatW & M ) const ALGLIN_OVERRIDE;
+
+    virtual
+    void
+    get_matrix_transposed( MatW & M ) const ALGLIN_OVERRIDE;
+
+    virtual
+    bool
+    foundNaN() const ALGLIN_OVERRIDE;
+
+    virtual
+    void
     get_data(
       integer   const * & pRows,
       integer   const * & pCols,
@@ -288,52 +523,6 @@ namespace alglin {
       pRows   = &this->rows.front();
       pCols   = &this->cols.front();
       pValues = &this->vals.front();
-    }
-
-    template <typename i_type, typename r_type>
-    void
-    export_data(
-      integer NNZ,
-      i_type  i[],
-      i_type  j[],
-      r_type  val[],
-      bool    fi
-    ) {
-      ALGLIN_ASSERT(
-        NNZ == this->rows.size() &&
-        NNZ == this->cols.size() &&
-        NNZ == this->vals.size(),
-        "export_data, bad dimension"
-      );
-      integer offs = 0;
-      if ( fi ) ++offs;
-      if ( this->fortran_indexing ) --offs;
-      for ( integer index = 0; index < NNZ; ++index ) {
-        i[index]   = i_type(this->rows[index]+offs);
-        j[index]   = i_type(this->cols[index]+offs);
-        val[index] = r_type(this->vals[index]);
-      }
-    }
-
-    template <typename i_type, typename r_type>
-    void
-    export_data(
-      std::vector<i_type> & i,
-      std::vector<i_type> & j,
-      std::vector<r_type> & v,
-      bool                  fi
-    ) {
-      integer offs = 0;
-      if ( fi ) ++offs;
-      if ( this->fortran_indexing ) --offs;
-      i.clear(); i.reserve( this->nnz );
-      j.clear(); j.reserve( this->nnz );
-      v.clear(); v.reserve( this->nnz );
-      for ( integer index = 0; index < this->nnz; ++index ) {
-        i.push_back( i_type(this->rows[index]+offs) );
-        j.push_back( i_type(this->cols[index]+offs) );
-        v.push_back( r_type(this->vals[index]) );
-      }
     }
 
     virtual
@@ -382,30 +571,65 @@ namespace alglin {
     :|:  \__,_\__,_\__,_|_|\__|_\___/_||_\__,_|_| |_|_|_\___|\__|_||_\___/\__,_/__/
     \*/
 
-    void clear();
+    template <typename i_type, typename r_type>
+    void
+    export_data(
+      integer NNZ,
+      i_type  i[],
+      i_type  j[],
+      r_type  val[],
+      bool    fi
+    ) {
+      ALGLIN_ASSERT(
+        NNZ == this->rows.size() &&
+        NNZ == this->cols.size() &&
+        NNZ == this->vals.size(),
+        "export_data, bad dimension"
+      );
+      integer offs = 0;
+      if ( fi ) ++offs;
+      if ( this->fortran_indexing ) --offs;
+      for ( integer index = 0; index < NNZ; ++index ) {
+        i[index]   = i_type(this->rows[index]+offs);
+        j[index]   = i_type(this->cols[index]+offs);
+        val[index] = r_type(this->vals[index]);
+      }
+    }
+
+    template <typename i_type, typename r_type>
+    void
+    export_data(
+      std::vector<i_type> & i,
+      std::vector<i_type> & j,
+      std::vector<r_type> & v,
+      bool                  fi
+    ) {
+      integer offs = 0;
+      if ( fi ) ++offs;
+      if ( this->fortran_indexing ) --offs;
+      i.clear(); i.reserve( this->nnz );
+      j.clear(); j.reserve( this->nnz );
+      v.clear(); v.reserve( this->nnz );
+      for ( integer index = 0; index < this->nnz; ++index ) {
+        i.push_back( i_type(this->rows[index]+offs) );
+        j.push_back( i_type(this->cols[index]+offs) );
+        v.push_back( r_type(this->vals[index]) );
+      }
+    }
 
     void
-    init(
+    setup_as_full_row_major(
       integer N,
       integer M,
-      integer reserve_nnz,
       bool    fi = false
     );
 
     void
-    transpose() {
-      this->rows.swap(this->cols);
-      std::swap( this->nRows, this->nCols );
-    }
-
-    void to_FORTRAN_indexing();
-    void to_C_indexing();
-
-    void
-    setup_as_full_row_major( integer N, integer M, bool fi = false );
-
-    void
-    setup_as_full_column_major( integer N, integer M, bool fi = false );
+    setup_as_full_column_major(
+      integer N,
+      integer M,
+      bool    fi = false
+    );
 
     void
     fill( valueType const V[], integer M );
@@ -415,39 +639,6 @@ namespace alglin {
 
     void
     reserve( integer reserve_nnz );
-
-    void
-    push_value_C(
-      integer   row,
-      integer   col,
-      valueType val
-    );
-
-    void
-    push_value_F(
-      integer   row,
-      integer   col,
-      valueType val
-    );
-
-    void
-    push_matrix(
-      integer        row_offs,
-      integer        col_offs,
-      Sparse const & Matrix,
-      bool           transpose = false
-    );
-
-    void
-    get_matrix( MatW & M ) const;
-
-    void
-    get_matrix_symmetric( MatW & M ) const;
-
-    void
-    get_matrix_transposed( MatW & M ) const;
-
-    bool foundNaN() const;
   };
 
   // explicit instantiation declaration to suppress warnings
