0ca99bc5c146910b369faded87bb48133690cdc1
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2020-02-11 22:00:05 +0100

    updated submodules

--- makefile_linux.mk
+++ makefile_linux.mk
@@ -29,7 +29,7 @@ endif
 
 ifneq (,$(findstring ALGLIN_USE_OPENBLAS,$(USED_LIB)))
   FPATH=$(dir $(shell gfortran -print-libgcc-file-name))
-  override LIBS += -lopenblas -L$(FPATH) -lgfortran
+  override LIBS += -lopenblas -lomp -L$(FPATH) -lgfortran
 endif
 
 ifneq (,$(findstring ALGLIN_USE_ATLAS,$(USED_LIB)))
