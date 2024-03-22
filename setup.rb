1487f1113986cdea9133ba552266ce73d41f1b01
Enrico Bertolazzi <enrico.bertolazzi@unitn.it>
2024-03-22 11:58:48 +0100

    corrected setup.rb

--- setup.rb
+++ setup.rb
@@ -1,11 +1,13 @@
 puts "Setup submodules"
+system('git submodule update --init --recursive --checkout')
 system('git submodule foreach --recursive git submodule init')
 system('git submodule foreach --recursive git submodule update')
 system('git submodule foreach --recursive git submodule sync')
-system('cd submodule/LapackWrapper; git checkout develop')
-system('cd submodules/LapackWrapper/submodules/Utils; git checkout main')
+system('cd submodules/LapackWrapper; git checkout develop')
+system('cd submodules/LapackWrapper/cmake_utils; git checkout main')
+system('cd submodules/LapackWrapper/submodules/Utils; git checkout master')
 system('cd submodules/LapackWrapper/submodules/Utils/cmake_utils; git checkout main')
-system('cd submodules/cmake_utils; git checkout main')
+system('cd cmake_utils; git checkout main')
 
 puts ARGV
 
